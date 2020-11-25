2fe79912d8020dd22a954cf4b938e74ad092d67c
yoootan <59093014+yoootan@users.noreply.github.com>
2020-11-25 15:30:23 +0900

    トップページ変更

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('customer_index');
 });
 //お客様用
 Route::get('/customer_index','CustomerController@customer_index');
